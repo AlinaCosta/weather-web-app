7715d951572961919124338c70a3be2d3b065293
Alina Costa <alinacosta87@gmail.com>
2023-12-19 23:19:32 +0200

    add responsive design

--- sections/CurrentWeather.js
+++ sections/CurrentWeather.js
@@ -25,8 +25,8 @@ function displayCurrentWeather(city) {
       let todayHighlights = document.querySelector('.today-highlights');
 
       currentWeather.innerHTML = `
-                <h1 class="display-5 m-0 mt-3">${name}</h1>
-                <p class="fs-5 mt-2">${day}, ${hours}</p>
+                <h1 class="display-4 m-0 mt-md-1 mt-3 city-name">${name}</h1>
+                <p class="fs-4 mt-2">${day}, ${hours}</p>
                 <div class="d-flex align-items-baseline gap-2 mt-3">
                   <span class="my-1"
                     ><i class="bi ${weatherIcon} display-1"></i
@@ -39,37 +39,37 @@ function displayCurrentWeather(city) {
             <h2 class="fs-6 mb-3 ms-1 text-uppercase text-white">
               Today's Highlights
             </h2>
-            <div class="d-flex flex-wrap gap-3 today-highlights__cards">
-              <div class="card flex-grow-1 w-25 text-center p-3">
+            <div class="d-flex flex-wrap gap-2 gap-xl-3 today-highlights__cards">
+              <div class="card text-center">
                 <p class="field-name text-start">Real Feel</p>
                 <p class="fs-1">
                   <i class="bi ${weatherIcon}"></i> ${realFeel}°C
                 </p>
-                <p>${weatherDescription}</p>
+                <p class="mb-0">${weatherDescription}</p>
               </div>
-              <div class="card flex-grow-1 w-25 text-center p-3">
+              <div class="card text-center">
                 <p class="field-name text-start">Wind status</p>
                 <p class="fs-2">${windSpeed} km/h</p>
                 <span><i class="bi bi-wind"></i></span>
               </div>
-              <div class="card flex-grow-1 w-25 text-center p-3">
+              <div class="card text-center">
                 <p class="field-name text-start">Sunrise & Sunset</p>
-                <p class="fs-5 mt-1">
+                <p class="fs-5 mt-1 mb-0">
                   <i class="bi bi-sunrise-fill pe-2"></i>${sunrise}
                 </p>
-                <p class="fs-5"><i class="bi bi-sunset-fill pe-2"></i>${sunset}</p>
+                <p class="fs-5 mb-0"><i class="bi bi-sunset-fill pe-2"></i>${sunset}</p>
               </div>
-              <div class="card flex-grow-1 w-25 text-center p-3">
+              <div class="card text-center">
                 <p class="field-name text-start">Humidity</p>
                 <p class="fs-2">${humidity}%</p>
                 <span><i class="bi bi-moisture"></i></span>
               </div>
-              <div class="card flex-grow-1 w-25 text-center p-3">
+              <div class="card text-center">
                 <p class="field-name text-start">Vizibility</p>
                 <p class="fs-2">${weatherVisibility} km</p>
                 <span><i class="bi bi-eye-fill"></i></span>
               </div>
-              <div class="card flex-grow-1 w-25 text-center p-3">
+              <div class="card text-center">
                 <p class="field-name text-start">Pressure</p>
                 <p class="fs-2">${pressure} mb</p>
                 <span><i class="bi bi-speedometer"></i></span>
